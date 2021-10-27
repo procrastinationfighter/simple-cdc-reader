27affc75e1ee2e9091554a21feec60ff7fe9ad03
Adam Boguszewski <ab417730@students.mimuw.edu.pl>
2021-10-28 00:21:05 +0200

    Fix bugs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use scylla::{SessionBuilder, Session};
 use scylla::frame::value::Timestamp;
+use scylla::statement::prepared_statement::PreparedStatement;
 use futures_util::StreamExt;
 
 // Assumption of the program:
@@ -16,7 +17,7 @@ const DURATION_TIME_IN_MS: i64 = 1_800_000;     // Default: 30 minutes durations
 // I tried to query this by using toTimestamp("cdc$time"), but utterly failed.
 const QUERY_STR: &str = "SELECT \"ck\", \"pk\", \"v\" FROM ks.t_scylla_cdc_log \
                             WHERE \"cdc$stream_id\" = ? \
-                                AND \"cdc$time\" >= maxTimeuuid(?)\
+                                AND \"cdc$time\" >= minTimeuuid(?) \
                                 AND \"cdc$time\" < minTimeuuid(?)";
 
 fn parse_arguments() -> Vec<u8> {
@@ -30,29 +31,28 @@ fn parse_arguments() -> Vec<u8> {
 }
 
 async fn query_data(session: &Session,
-                    stream_id: &Vec<u8>,
-                    next_starting_point: chrono::Duration) -> Result<i64, Box<dyn std::error::Error>> {
+                    stream_id: &[u8],
+                    next_starting_point: chrono::Duration,
+                    prepared_query: &PreparedStatement) -> Result<i64, Box<dyn std::error::Error>> {
     // The function assumes that the program slept,
-    // so that next_starting_point in ms is smaller than
+    // so that next_starting_point in ms should be smaller than
     // now - NEW_DATA_DELAY_IN_MS
+    // If it's not, return the old next_starting_point value.
     let right_timestamp_ms = {
         let now = chrono::Local::now().timestamp_millis();
         let new_time = next_starting_point.num_milliseconds() + DURATION_TIME_IN_MS;
 
-        if new_time >= now - NEW_DATA_DELAY_IN_MS {
-            now - NEW_DATA_DELAY_IN_MS
-        } else {
-            new_time
+        if now - NEW_DATA_DELAY_IN_MS < next_starting_point.num_milliseconds() {
+            return Ok(next_starting_point.num_milliseconds());
         }
+
+        std::cmp::min(new_time, now - NEW_DATA_DELAY_IN_MS)
     };
     let right_timestamp = chrono::Duration::milliseconds(right_timestamp_ms);
 
-    let prepared_query = session
-        .prepare(QUERY_STR)
-        .await?;
-
     let mut rows_stream = session
-        .execute_iter(prepared_query, (stream_id, Timestamp(next_starting_point), Timestamp(right_timestamp)))
+        .execute_iter(prepared_query.clone(),
+                      (stream_id.to_vec(), Timestamp(next_starting_point), Timestamp(right_timestamp)))
         .await?
         .into_typed::<(Option<i32>, Option<i32>, Option<i32>,)>();
 
@@ -72,9 +72,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build()
         .await?;
     let mut starting_point = chrono::Duration::milliseconds(chrono::Local::now().timestamp_millis() - STARTING_TIME_DELAY_IN_MS);
+    let prepared_query = session
+        .prepare(QUERY_STR)
+        .await?;
 
     loop {
-        let starting_point_ms = query_data(&session, &stream_id, starting_point).await?;
+        let starting_point_ms = query_data(&session, &stream_id, starting_point, &prepared_query).await?;
         starting_point = chrono::Duration::milliseconds(starting_point_ms);
         tokio::time::sleep(tokio::time::Duration::from_millis(WAIT_TIME_IN_MS)).await;
     }
